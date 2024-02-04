68d59e795dc9d08ace2a24779484c00c2d8f24fb
CheechRockwizard <136230172+LamcatUK@users.noreply.github.com>
2024-02-04 18:19:53 +0000

    Update sessions_cards.php

--- page-templates/blocks/sessions_cards.php
+++ page-templates/blocks/sessions_cards.php
@@ -1,12 +1,12 @@
 <section class="sessions_cards">
     <div class="container-xl">
-        <div class="row">
+        <div class="row g-3 justify-content-center">
             <?php
     if(have_rows('sessions')) {
         while(have_rows('sessions')) {
             the_row();
             ?>
-            <div class="col-md-6 col-lg-3 pb-5">
+            <div class="col-md-6 col-lg-3 pb-3">
                 <a href="<?=get_sub_field('page')?>"
                     class="sessions_cards__card washi">
                     <img src="<?=wp_get_attachment_image_url(get_sub_field('image'), 'large')?>"
